69abe4f3fb040bd3c2ee4bb544c2059a2e6a63b9
bumsuejung <y011010@naver.com>
2019-12-30 17:19:10 +0900

    Updates

---
+++ precache-manifest.ae27eddeaec85f4b2ae50bc204488225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792c47dfd1a057abe43aeb2fea50cf73",
+    "url": "/React_todo-app/index.html"
+  },
+  {
+    "revision": "3e94d3ea27c92c8b56c6",
+    "url": "/React_todo-app/static/css/main.d7c9569e.chunk.css"
+  },
+  {
+    "revision": "3b24d0a27b197cc252fc",
+    "url": "/React_todo-app/static/js/2.a31e61ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React_todo-app/static/js/2.a31e61ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e94d3ea27c92c8b56c6",
+    "url": "/React_todo-app/static/js/main.5a304672.chunk.js"
+  },
+  {
+    "revision": "ed8cf2ceab3ca7f5d28a",
+    "url": "/React_todo-app/static/js/runtime-main.bdbfd77a.js"
+  }
+]);
